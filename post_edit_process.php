263f2537392668b26b1f3e0f00009c9599df4d52
soyunkim1213 <soyunkim1213@pusan.ac.kr>
2025-12-02 19:28:47 +0900

    게시글_기능
    수정된 게시글 데이터를 DB에 업데이트하는 파일

---
+++ post_edit_process.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'auth_guard.php';
+require_once 'db.php';
+
+$id = $_POST['id'];
+$title = $_POST['title'];
+$content = $_POST['content'];
+$rating = $_POST['rating'];
+
+$stmt = $pdo->prepare("UPDATE posts SET title=?, content=?, rating=? WHERE id=? AND user_id=?");
+$stmt->execute([$title, $content, $rating, $id, $_SESSION['user_id']]);
+
+echo "<script>
+        alert('수정 완료!');
+        window.location.href='post_view.php?id=$id';
+      </script>";
+?>
